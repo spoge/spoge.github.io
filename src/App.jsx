63eb660ce20a5389182a939adff32f7be7c9f6ba
spoge <sondreboge@hotmail.com>
2020-05-12 11:32:29 +0200

    Re-init website but in React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>This is my personal website</div>
+    </div>
+  );
+}
+
+export default App;
