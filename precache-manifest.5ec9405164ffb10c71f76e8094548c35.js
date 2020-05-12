780bf7979078eca08028561acc7839995a364dd7
spoge <sondreboge@hotmail.com>
2020-05-12 13:04:42 +0200

    Updates

---
+++ precache-manifest.5ec9405164ffb10c71f76e8094548c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551a7d121c7c63163a5344ddb34e6053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75ef0beca11668a55af2",
+    "url": "/static/css/main.f0f205b6.chunk.css"
+  },
+  {
+    "revision": "aff777a88b73c475c20e",
+    "url": "/static/js/2.b0211ea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0211ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ef0beca11668a55af2",
+    "url": "/static/js/main.ea67410e.chunk.js"
+  },
+  {
+    "revision": "17f65b35950b2af45ffa",
+    "url": "/static/js/runtime-main.9dce1b9d.js"
+  }
+]);
