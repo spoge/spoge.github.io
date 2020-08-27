658356d44b5a3262bab53aed029ee343f102ade9
Sondre Sparby Boge <sondreboge@hotmail.com>
2020-08-27 12:54:08 +0200

    Updates

---
+++ precache-manifest.1d6f59993e0335998ae7fed6a6832cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15172ea766490435c426c07d28ddfce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f344dcebfe6e2dae4fb",
+    "url": "/static/css/main.79a11359.chunk.css"
+  },
+  {
+    "revision": "65fca0abc8f44ad48442",
+    "url": "/static/js/2.72f8b213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72f8b213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f344dcebfe6e2dae4fb",
+    "url": "/static/js/main.a39925e7.chunk.js"
+  },
+  {
+    "revision": "2d9aedd9ef94aba23610",
+    "url": "/static/js/runtime-main.2790e558.js"
+  },
+  {
+    "revision": "9005b95b68f11d5f5c368798294fd781",
+    "url": "/static/media/ScreenMatrix.9005b95b.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbe9283aab8c3fdc5c6cdbe384a36d29.js"
+  "/precache-manifest.1d6f59993e0335998ae7fed6a6832cf2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
