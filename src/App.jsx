de578936532d1dfb7aed77d6b82884f5917b29ca
Sondre Sparby Boge <sondreboge@hotmail.com>
2020-10-06 15:12:43 +0200

    Split redirecting from Terminal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
-import Terminal from "./components/Terminal";
-import TerminalCommand from "./components/TerminalCommand";
+import TerminalWindow from "./components/TerminalWindow";
 
-const validPaths = ["life", "pokemon", "/"];
-const validPathsAlias = [{ home: "/", conway: "life", pokebox: "pokemon" }];
-
-const App = ({ history }) => {
-  const [inputHistory, setInputHistory] = useState([]);
-  const [displayHistory, setDisplayHistory] = useState([
-    <div>
-      Enter <b>help</b> to see all available commands.
-    </div>,
-  ]);
-
-  const [brainfuckMode, setBrainfuckMode] = useState(false);
-
-  const onTerminalEnter = (text, ignoreCommand = false) => {
-    /**
-     * Handling terminal input:
-     * 1. Ignore this command?
-     * 2. Redirect with exact match? (i.e. "/fun" => "/fun")
-     * 3. Redirect with alias? (i.e. "/home" => "/")
-     * 4. Clear terminal?
-     * 5. Brainfuck interpreter?
-     * 6. Cool commands in terminal!
-     */
-    setInputHistory([...inputHistory, text]);
-    if (ignoreCommand) {
-      setDisplayHistory([
-        ...displayHistory,
-        <TerminalCommand
-          history={history}
-          text={text}
-          brainfuckMode={brainfuckMode}
-          ignoreCommand={ignoreCommand}
-        />,
-      ]);
-    } else if (validPaths.includes(text)) {
-      history.push(text);
-    } else if (
-      validPathsAlias.flatMap((entry) => Object.keys(entry)).includes(text)
-    ) {
-      history.push(validPathsAlias.find((entry) => entry[text])[text]);
-    } else if (text === "clear") {
-      setDisplayHistory([]);
-    } else if (text === "brainfuck") {
-      setDisplayHistory([
-        ...displayHistory,
-        <TerminalCommand
-          history={history}
-          text={text}
-          brainfuckMode={!brainfuckMode}
-        />,
-      ]);
-      setBrainfuckMode(!brainfuckMode);
-    } else {
-      setDisplayHistory([
-        ...displayHistory,
-        <TerminalCommand
-          history={history}
-          text={text}
-          brainfuckMode={brainfuckMode}
-        />,
-      ]);
-    }
-  };
-
+const App = () => {
   return (
     <div className="app">
       <Switch>
@@ -77,12 +13,11 @@ const App = ({ history }) => {
         <Route path="/pokemon">
           <Pokemon />
         </Route>
+        {/* <Route path="/terminal">
+          <TerminalWindow />
+        </Route> */}
         <Route path="/">
-          <Terminal
-            displayHistory={displayHistory}
-            inputHistory={inputHistory}
-            onEnterPress={onTerminalEnter}
-          />
+          <TerminalWindow />
         </Route>
       </Switch>
     </div>
