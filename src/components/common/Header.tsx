b139f349ca055ad7df2708da38e3f4edbcd8cb2e
spoge <spoge@users.noreply.github.com>
2022-02-03 14:28:20 +0100

    Underline header navigation items

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,22 +1,41 @@
-import React from "react";
 // eslint-disable-next-line
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 import GithubIcon from "./GithubIcon";
 import "./styles/Header.scss";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <div className="header">
       <div className="header-bar">
         <Link to="/">
-          <div className="header-logo">sboge.no</div>
+          <div
+            className={`header-logo ${
+              location.pathname === "/" ? "selected" : ""
+            }`}
+          >
+            sboge.no
+          </div>
         </Link>
         <div className="header-menu">
           <Link to="/blog">
-            <div className="header-menu-item">Blog</div>
+            <div
+              className={`header-menu-item ${
+                location.pathname.startsWith("/blog") ? "selected" : ""
+              }`}
+            >
+              Blog
+            </div>
           </Link>
           <Link to="/projects">
-            <div className="header-menu-item">Projects</div>
+            <div
+              className={`header-menu-item ${
+                location.pathname.startsWith("/projects") ? "selected" : ""
+              }`}
+            >
+              Projects
+            </div>
           </Link>
           <Link to={{ pathname: "https://github.com/spoge" }} target="_blank">
             <GithubIcon />
