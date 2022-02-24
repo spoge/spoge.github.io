997dfb382ea0a606d335bd3a41744380e77fa913
spoge <spoge@users.noreply.github.com>
2022-02-24 14:54:56 +0100

    Small refactor & fix dev/non-dev dependencies

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,18 +5,20 @@ import Background from "./common/Background";
 
 const Blog = () => {
   return (
-    <Link to="/blog/gba-modding">
-      <Card
-        image={`${process.env.PUBLIC_URL}/img/gba-modding/gba_new_shell.jpeg`}
-        circularImage={true}
-        title="GBA modding!"
-        text="Click here to build the ultimate Gameboy Advance."
-        onClick={() => {}}
-      />
+    <>
+      <Link to="/blog/gba-modding">
+        <Card
+          image={`${process.env.PUBLIC_URL}/img/gba-modding/gba_new_shell.jpeg`}
+          circularImage={true}
+          title="GBA modding!"
+          text="Click here to build the ultimate Gameboy Advance."
+          onClick={() => {}}
+        />
+      </Link>
       <Background
         image={`${process.env.PUBLIC_URL}/img/bg/gba_background.jpeg`}
       />
-    </Link>
+    </>
   );
 };
 
